6952acf21892e886ad6f0d81547cfec641009e6d
Kudrat Mullani <75575175+kudratmullani@users.noreply.github.com>
2026-02-03 23:58:52 +0530

    Update talker.py

--- ros2_ws/src/demo_pkg/demo_pkg/talker.py
+++ ros2_ws/src/demo_pkg/demo_pkg/talker.py
@@ -1,23 +1,18 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
 
 class SimpleTalker(Node):
     def __init__(self):
         super().__init__('simple_talker')
-        self.publisher = self.create_publisher(String, 'chatter', 10)
-        self.timer = self.create_timer(1.0, self.publish_message)
+        self.timer = self.create_timer(1.0, self.timer_callback)
 
-    def publish_message(self):
-        msg = String()
-        msg.data = 'Hello from ROS 2 Jazzy Docker!'
-        self.publisher.publish(msg)
-        self.get_logger().info(msg.data)
+    def timer_callback(self):
+        self.get_logger().info('Hello from ROS 2 Jazzy Docker!')
 
 def main():
     rclpy.init()
     node = SimpleTalker()
-    rclpy.spin(node)
+    rclpy.spin(node)   # 👈 THIS MUST EXIST
     rclpy.shutdown()
 
 if __name__ == '__main__':
